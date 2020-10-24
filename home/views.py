4c8a42c5d6c1f55d6cc7f4a80b8ce06768001519
unknown <you@example.comviet.dinh217@gmail.com>
2020-10-24 10:42:58 +0700

    update blog

--- home/views.py
+++ home/views.py
@@ -4,7 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    response = HttpResponse()
-    response.writelines("<h1>Welcome</h1>")
-    response.write("Đây là app home")
-    return response
+   return render(request,'pages/home.html')
