6745fc92ad5351db32074e0d5cbf03e1b4b9688e
unknown <you@example.comviet.dinh217@gmail.com>
2020-10-31 10:55:22 +0700

    update blog

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length = 100)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
